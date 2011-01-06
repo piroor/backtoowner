c158091a8ef445b829feb46f4bbdaf0e25e59029
SHIMODA Hiroshi <shimoda@clear-code.com>
2011-01-06 13:01:29 +0900

    activate back button if there is an owner

--- modules/backtoowner.js
+++ modules/backtoowner.js
@@ -8,6 +8,11 @@ BackToOwner.prototype = {
 	
 /* Utilities */ 
 	
+	get browser() 
+	{
+		return this._window.gBrowser;
+	},
+	
 	get backCommand() 
 	{
 		return this._window.document.getElementById('Browser:Back');
@@ -26,6 +31,7 @@ BackToOwner.prototype = {
 		this._window = aWindow;
 
 		this._window.addEventListener('unload', this, false);
+		this.browser.addProgressListener(this, Ci.nsIWebProgress.NOTIFY_ALL);
 		this.initCommand(this.backCommand);
 		this.initCommand(this.backOrDuplicateCommand);
 	},
@@ -33,6 +39,7 @@ BackToOwner.prototype = {
 	destroy : function() 
 	{
 		this._window.removeEventListener('unload', this, false);
+		this.browser.removeProgressListener(this);
 		this.destroyCommand(this.backCommand);
 		this.destroyCommand(this.backOrDuplicateCommand);
 		this._window = null;
@@ -44,7 +51,8 @@ BackToOwner.prototype = {
 	{
 		if (!aCommand) return;
 
-		aCommand.addEventListener('DOMAttrModified', this, false);
+		this.updateCommand(aCommand);
+
 		aCommand.addEventListener('command', this, true);
 	},
  
@@ -52,10 +60,31 @@ BackToOwner.prototype = {
 	{
 		if (!aCommand) return;
 
-		aCommand.removeEventListener('DOMAttrModified', this, false);
 		aCommand.removeEventListener('command', this, true);
+
+		if (this.browser.canGoBack)
+			aCommand.removeAttribute('disabled');
+		else
+			aCommand.setAttribute('disabled', true);
 	},
- 
+
+	updateCommand : function(aCommand)
+	{
+		if (
+			aCommand &&
+			!this.browser.canGoBack &&
+			this.getOwner(this.browser.selectedTab)
+			)
+			aCommand.removeAttribute('disabled');
+	},
+
+	getOwner : function(aTab)
+	{
+		var w = this._window;
+		return !aTab ? null :
+				'TreeStyleTabService' in w ? w.TreeStyleTabService.getParentTab(aTab) :
+				aTab.owner || null ;
+	},
  
 /* event handling */ 
 	
@@ -66,22 +95,39 @@ BackToOwner.prototype = {
 			case 'unload':
 				return this.destroy();
 
-			case 'DOMAttrModified':
-				return this.onChangeAttribute(aEvent);
-
 			case 'command':
 				return this.onCommand(aEvent);
 		}
 	},
 
-	onChangeAttribute : function(aEvent)
-	{
-		dump('onChangeAttribute\n');
-	},
-
 	onCommand : function(aEvent)
 	{
 		dump('onCommand\n');
+	},
+
+/* nsIWebProgressListener */
+	onStateChange : function() {},
+	onProgressChange : function() {},
+	onStatusChange : function() {},
+	onSecurityChange : function() {},
+	onLocationChange : function(aWebProgress, aRequest, aLocation)
+	{
+		this.updateCommand(this.backCommand);
+		this.updateCommand(this.backOrDuplicateCommand);
+	},
+
+/* nsIWebProgressListener2 */
+	onProgressChange64 : function() {},
+	onRefreshAttempted : function() { return true; },
+
+/* nsISupports */
+	QueryInterface : function (aIID)
+	{
+		if (aIID.equals(Ci.nsIWebProgressListener) ||
+			aIID.equals(Ci.nsIWebProgressListener2) ||
+			aIID.equals(Ci.nsISupports))
+			return this;
+		throw Cr.NS_NOINTERFACE;
 	}
   
 };
